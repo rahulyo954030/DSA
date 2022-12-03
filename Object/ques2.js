debcbacdd99a7041e9cafdbc3711e6be17541192
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-03 23:41:45 +0530

    Done Lucky Number

--- Object/ques2.js
+++ Object/ques2.js
@@ -1,3 +1,23 @@
 // Lucky number
 
-let arr = [1, 2, 2, 3,3,5 ]
+let arr = [1, 2, 2, 3,3,4,5,5,5,5,5 ]
+
+let obj ={}
+
+for(let i=0; i<arr.length; i++){
+    if(obj[arr[i]]==undefined){
+        obj[arr[i]]=1
+    }
+    else{
+        obj[arr[i]]++
+    }
+}
+
+let max = -1
+for(let key in obj){
+    if(obj[key]==key && obj[key]>max){
+        max = obj[key]
+    }
+    
+}
+console.log(max);
