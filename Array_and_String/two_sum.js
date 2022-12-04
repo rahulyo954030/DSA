6b8b55a334f2e2c021c95aaceeea682e54410647
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-04 23:21:53 +0530

    doe two sum

--- Array_and_String/two_sum.js
+++ Array_and_String/two_sum.js
@@ -1,7 +1,12 @@
-let arr = [1,2,8,3,8,5], k = 8
+let arr = [1,2,8,3,8,5], k = 10
 
+let flag =false
 for(let i =0; i<arr.length; i++){
     for(let j =i+1; j<arr.length; j++){
-        
+        if(arr[i]+arr[j]==k){
+            flag = true
+        }
     }
 }
+
+console.log(flag);
