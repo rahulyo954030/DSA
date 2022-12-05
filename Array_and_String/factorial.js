0e8d114b98faca7ec7729f0340f9a1fa49a12b5a
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-05 19:03:18 +0530

    done factorial

--- Array_and_String/factorial.js
+++ Array_and_String/factorial.js
@@ -1,6 +1,18 @@
-let num = 5
+//<---------------------Using Loop------------------------------>
+let num = 8
 let sum = 1
 for(let i =1; i<=num; i++){
     sum = sum *i
 }
 console.log(sum);
+//<---------------------Using Loop------------------------------>
+
+//<---------------------Recursion------------------------------>
+function factorial(num){
+    if(num<=1){
+        return 1
+    }
+    return num * factorial(num-1)
+}
+console.log(factorial(8));
+//<---------------------Recursion------------------------------>
