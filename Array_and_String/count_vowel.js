f535bb991a86e91640353a3f3a15238f49f11df9
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-08 00:24:16 +0530

    done count vowel

---
+++ Array_and_String/count_vowel.js
@@ -0,0 +1,10 @@
+let str = "rahulsinghaio"
+let vowel = "aeiou"
+
+let count =0
+for(let i =0; i<str.length; i++){
+    if(str[i]== "a" || str[i]== "e" ||str[i]== "i" ||str[i]== "o" ||str[i]== "u"){
+        count++
+    }
+}
+console.log(count);
