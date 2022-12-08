4035bb465fdd6ea81beb7f0174c71d5ac1b49329
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-08 23:39:53 +0530

    done

--- Array_and_String/count_vowel.js
+++ Array_and_String/count_vowel.js
@@ -8,3 +8,14 @@ for(let i =0; i<str.length; i++){
     }
 }
 console.log(count);
+
+// or
+
+https://codesandbox.io/s/mern-q1-forked-tdxhb0?file=/src/index.js
+
+for(let i =0; i<str.length; i++){
+    if(str[i]== "a" || str[i]== "e" ||str[i]== "i" ||str[i]== "o" ||str[i]== "u"){
+        count = count+1
+    }
+}
+console.log(count);
