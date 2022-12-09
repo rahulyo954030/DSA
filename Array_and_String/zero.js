6c45dfd419b5ed8195599c5e16456bdfad889b39
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-09 23:58:19 +0530

    done

--- Array_and_String/zero.js
+++ Array_and_String/zero.js
@@ -1,8 +1,17 @@
 let arr =[4,5,8,1,0,9,0,3]
 
 // output = [4,5,8,1,9,3,0,0]
-
+let count =0
 for(let i =0; i<arr.length; i++){
-   
+   if(arr[i]==0){
+    count++
+    arr.splice(i,1)
+   }
 }
+
+for(let j =0; j<count; j++){
+    arr.push(0)
+ }
+
+
 console.log(arr);
